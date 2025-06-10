ed81bc62cd0b5e009e4b2e382e0394ed5d25bbe8
Ammon F. <ammonfasusi@gmail.com>
2025-06-10 15:52:01 -0400

    Update pingLab.py

--- pingLab.py
+++ pingLab.py
@@ -1,17 +1,11 @@
 import subprocess
 
 #Global Variable
-
-
+result = subprocess.run(["ping",  "google.com"], capture_output=True, text=True, check=True]) #Make a simpler global variable
+    
 try: 
-    result = subprocess.run(["ping",  "google.com"], capture_output=True, text=True, check=True]) #Make a simpler global variable
     print(result) 
 
 except:
     subprocess.CalledProcessError 	
     print(result.stderr)
-  
-result = subprocess.run(["ping",  "google.com"], capture_output=True, text=True, check=True)
-
-
-
