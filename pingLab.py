1223021d14d60cec063125d62a3e2506d9bd1a88
Ammon F. <ammonfasusi@gmail.com>
2025-08-13 11:40:23 -0400

    Add files via upload

--- pingLab.py
+++ pingLab.py
@@ -1,6 +1,11 @@
+from pathlib import Path
 import subprocess
+import requests
+import pprint
+import shutil
 
-# Global Variable
+#Test connections with ping and requests
+
 result = subprocess.run(
     ["ping", "google.com"],
     stdout=subprocess.PIPE,
@@ -8,9 +13,33 @@ result = subprocess.run(
     text=True,
     check=True
 )
+response = requests.get("https://www.google.com")
+
+html = response.text
+
+#Exception handling for error/ status codes 
 
 try:
-    print(result)
+    print(result, "\n", response)
+   
 
 except subprocess.CalledProcessError as e:
-    print(result.stderr)
+    print(result.stderr, {e})
+    print(response.status_code, {e})
+
+#Cleanly print Huge data and save it to a .txt
+pprint.pprint(data)
+
+
+path = C:\Users\Dell\OneDrive\Documents\py
+folder / urloutput.txt
+
+with open("html", "w", encoding="UTF-8") as urloutput:
+    datalog.write(f"Status: {response.status_code}")
+   
+
+
+
+
+
+
