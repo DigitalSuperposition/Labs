2b4f301c3141343bf6a6f66d7a5f7073194fcf17
Ammon F. <ammonfasusi@gmail.com>
2025-10-03 13:01:27 -0400

    \emaillab.py

--- emaillab.py
+++ emaillab.py
@@ -24,6 +24,15 @@ receiver_email = "ammon.fasusi.ctr@marines.usmc.mil"
 subject = "python test email"
 body = "This is a test email sent from python script,if this goes through, congratulations, this means that the email sent through script was sucessful."
 
+""" msg = EmailMessage()
+
+msg['From'] = "Python Script <rcen.@marines.usmc.mil>"
+msg['To'] = receiver_email
+msg['subject'] = subject
+msg['X-Mailer'] = "Python smtplib"
+msg.set_content(body + "\n Sent using Python smtplib") """
+
+print(response, traf_res )
 
 
 
