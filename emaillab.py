6c5b890f1518003dea24134e1d01a89a8cf9e207
Ammon F. <ammonfasusi@gmail.com>
2025-10-25 17:48:03 -0400

    emaillab.py
    Finalize project email from python template.

--- emaillab.py
+++ emaillab.py
@@ -1,38 +1,48 @@
 ﻿from smtplib import SMTP
 from email.message import EmailMessage
+#from getpass import getpass #encrypt
 import smtplib
 import datetime
 import schedule
 import win32com.client
+import platform
 
-
-
-#Server configuration /authenication
-with smtplib.SMTP.connect(host='', port=) as server:
-    server.starttls()
-    server.ehlo()
-    server.starttls(context=context)
-    server.ehlo()
-    server.login(sender_email, input("Enter pin:"))
-    server.quit()
-	
-namespace = outlook.GetNamespace("MAPI")
 
 #EmailMessage information
-sender_email = "ammon.fasusi.ctr@marines.usmc.mil"
-receiver_email = "ammon.fasusi.ctr@marines.usmc.mil"
+sender_email = "first.last@domain.tld"
+receiver_email = "first.last@domain.tld"
 subject = "python test email"
-body = "This is a test email sent from python script,if this goes through, congratulations, this means that the email sent through script was sucessful."
+body = """This is a test email sent from python script,if this goes through, congratulations, this means that the email sent through script was sucessful."""
 
-""" msg = EmailMessage()
+msg = EmailMessage()
 
-msg['From'] = "Python Script <rcen.@marines.usmc.mil>"
+python_info = f"Python/{platform.python_version()} ({platform.system()} {platform.release()})"
+
+msg['From'] = sender_email
 msg['To'] = receiver_email
 msg['subject'] = subject
-msg['X-Mailer'] = "Python smtplib"
-msg.set_content(body + "\n Sent using Python smtplib") """
+msg['X-Python-Info'] = python_info
+msg.set_content(
+    body + f"\nPython {platform.python_version()} ({platform.system()} {platform.release()})" #Platform and OS in email.
+)
 
-print(response, traf_res )
+try:
+#Connect to relay server, server type using, All available IPv4/6
+    with smtplib.SMTP(host='IP address or server name resolve', port=#) as server:
+        server.set_debuglevel(1)    #Enable debug output.
+        response = server.ehlo()
+        server.starttls()
+        #server.login("username", "pin or password") #If login is required.
+        response = server.ehlo()
+        server.send_message(msg)
+        #server.quit()
+
+except Exception as e:
+    print(f"General error: {e}")
+
+print("EHLO response", response )
+
+
 
 
 
